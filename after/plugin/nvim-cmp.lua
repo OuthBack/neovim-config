e1233bfad85b31fb6822c1ffd5417bc94302adb4
Henrique Figueiredo <riquessan@gmail.com>
2026-04-13 18:01:14 -0300

    update

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -31,7 +31,7 @@ cmp.setup({
     mapping = mapping,
     enabled = function()
         return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt"
-        or require("cmp_dap").is_dap_buffer()
+        -- or require("cmp_dap").is_dap_buffer()
     end
 })
 
