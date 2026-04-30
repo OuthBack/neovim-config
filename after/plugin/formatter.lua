7270cc01eeda50b9cf8f292e23994ac982ff7e68
Henrique Figueiredo <riquessan@gmail.com>
2026-04-30 14:26:01 -0300

    update

--- after/plugin/formatter.lua
+++ after/plugin/formatter.lua
@@ -144,7 +144,6 @@ local formatterConfig = {
         return {
             exe = "gofmt",
             args = {
-                ">",
                 util.escape_path(util.get_current_buffer_file_name()),
             },
             stdin = true,
